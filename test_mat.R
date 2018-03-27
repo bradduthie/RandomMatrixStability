fff5052b87fbc47a07991a585adf1232c9e44a10
Brad Duthie <brad.duthie@gmail.com>
2018-03-27 16:26:02 +0100

    New plot and data read into

--- test_mat.R
+++ test_mat.R
@@ -78,7 +78,7 @@ summarise_randmat <- function(tot_res, fea_res){
         feasibl   <- fea_res[[i]][,1] == TRUE  & fea_res[[i]][,2] == TRUE;
         unfeased  <- fea_res[[i]][,1] == TRUE  & fea_res[[i]][,2] == FALSE;
         feased    <- fea_res[[i]][,1] == FALSE & fea_res[[i]][,2] == TRUE;
-        all_res[i, 1] <- i;
+        all_res[i, 1] <- i + 1;
         all_res[i, 2] <- sum(unstables);
         all_res[i, 3] <- sum(stables);
         all_res[i, 4] <- sum(unstabled);
@@ -90,6 +90,107 @@ summarise_randmat <- function(tot_res, fea_res){
     }
     return(all_res);
 }
+
+
+# Get:
+# (1) proportion stable made unstable
+# (2) proportion unstable made stable
+# (3) proportion feasible made infeasible
+# (4) proportion infeasible made feasible
+
+
+random      <- read.csv(file = "sim_results/random_community.csv");
+competition <- read.csv(file = "sim_results/competitor_community.csv");
+mutualism   <- read.csv(file = "sim_results/mutualist_community.csv");
+pred_prey   <- read.csv(file = "sim_results/predator_prey_community.csv");
+
+
+
+dat <- pred_prey;
+
+pr_destab <- dat[,4] / (dat[,4] + dat[,3]);
+pr_stabld <- dat[,5] / (dat[,5] + dat[,2]);
+pr_defeas <- dat[,8] / (dat[,8] + dat[,7]);
+pr_feased <- dat[,9] / (dat[,9] + dat[,6]);
+Sp        <- dat[,1];
+
+plot(dat[,1], pr_destab, type = "l", lwd = 2, ylim = c(0, 1));
+points(dat[,1], pr_stabld, type = "l", lwd = 2, col = "red");
+points(dat[,1], pr_defeas, type = "l", lwd = 2, col = "black", lty = "dashed");
+points(dat[,1], pr_feased, type = "l", lwd = 2, col = "red", lty = "dashed");
+
+
+
+# ==============================================================================
+par(mfrow = c(2, 2), mar = c(4, 4, 1, 1));
+# ==============================================================================
+dat      <- random;
+stab_nov <- dat[,3] + dat[,4];
+stab_var <- dat[,3] + dat[,5];
+feas_nov <- dat[,7] + dat[,8];
+feas_var <- dat[,7] + dat[,9];
+plot(x = Sp, y = stab_nov / 100000, type = "l", lwd = 2, ylim = c(0,1),
+     cex.lab = 1.5, cex.axis = 1.5, xlab = "Species Number (S)",
+     ylab = "Proportion stable or feasible");
+points(x = Sp, y = stab_var / 100000, type = "l", lwd = 2, lty = "dashed");
+points(x = Sp, y = feas_nov / 100000, type = "l", lwd = 2, col = "red");
+points(x = Sp, y = feas_var / 100000, type = "l", lwd = 2, col = "red",
+       lty = "dashed");
+legend(x = 40, y = 0.9, col = c("black", "black", "red", "red"), cex = 1.2,
+       legend = c(expression(paste("Stable, no Var(",gamma,")")),
+                             expression(paste("Stable, Var(",gamma,")")),
+                             expression(paste("Feasible, no Var(",gamma,")")),
+                             expression(paste("Feasible, Var(",gamma,")"))),
+       lty = c("solid", "dashed", "solid", "dashed"), lwd = 2);
+text(x = 60, y = 0.98, labels = "Random", cex = 2);
+# ==============================================================================
+dat      <- competition;
+stab_nov <- dat[,3] + dat[,4];
+stab_var <- dat[,3] + dat[,5];
+feas_nov <- dat[,7] + dat[,8];
+feas_var <- dat[,7] + dat[,9];
+plot(x = Sp, y = stab_nov / 100000, type = "l", lwd = 2, ylim = c(0,1),
+     cex.lab = 1.5, cex.axis = 1.5, xlab = "Species Number (S)",
+     ylab = "Proportion stable or feasible");
+points(x = Sp, y = stab_var / 100000, type = "l", lwd = 2, lty = "dashed");
+points(x = Sp, y = feas_nov / 100000, type = "l", lwd = 2, col = "red");
+points(x = Sp, y = feas_var / 100000, type = "l", lwd = 2, col = "red",
+       lty = "dashed");
+text(x = 60, y = 0.98, labels = "Competitive", cex = 2);
+# ==============================================================================
+dat      <- mutualism;
+stab_nov <- dat[,3] + dat[,4];
+stab_var <- dat[,3] + dat[,5];
+feas_nov <- dat[,7] + dat[,8];
+feas_var <- dat[,7] + dat[,9];
+plot(x = Sp, y = stab_nov / 100000, type = "l", lwd = 2, ylim = c(0,1),
+     cex.lab = 1.5, cex.axis = 1.5, xlab = "Species Number (S)",
+     ylab = "Proportion stable or feasible");
+points(x = Sp, y = stab_var / 100000, type = "l", lwd = 2, lty = "dashed");
+points(x = Sp, y = feas_nov / 100000, type = "l", lwd = 2, col = "red");
+points(x = Sp, y = feas_var / 100000, type = "l", lwd = 2, col = "red",
+       lty = "dashed");
+text(x = 60, y = 0.98, labels = "Mutualist", cex = 2);
+# ==============================================================================
+dat      <- pred_prey;
+stab_nov <- dat[,3] + dat[,4];
+stab_var <- dat[,3] + dat[,5];
+feas_nov <- dat[,7] + dat[,8];
+feas_var <- dat[,7] + dat[,9];
+plot(x = Sp, y = stab_nov / 100000, type = "l", lwd = 2, ylim = c(0,1),
+     cex.lab = 1.5, cex.axis = 1.5, xlab = "Species Number (S)",
+     ylab = "Proportion stable or feasible");
+points(x = Sp, y = stab_var / 100000, type = "l", lwd = 2, lty = "dashed");
+points(x = Sp, y = feas_nov / 100000, type = "l", lwd = 2, col = "red");
+points(x = Sp, y = feas_var / 100000, type = "l", lwd = 2, col = "red",
+       lty = "dashed");
+text(x = 60, y = 0.98, labels = "Predator-prey", cex = 2);
+# ==============================================================================
+
+
+
+
+
 
 
 
