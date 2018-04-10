68ff4dfcd37f6c60c01e84d16b44cc45e8955732
Brad Duthie <brad.duthie@gmail.com>
2018-04-10 13:10:58 +0100

    Add some code to look at different distrbutions of gamma

--- test_mat.R
+++ test_mat.R
@@ -1093,42 +1093,53 @@ points(xx, yy, type = "l", col = "blue")
 
 ################################################################################
 # GENETIC ALGORITHM:
+
 nn       <- 24;
 A1_dat   <- rnorm(n = nn * nn, mean = 0, sd = 0.4);
 A1       <- matrix(data = A1_dat, nrow = nn, ncol = nn);
 A1       <- species_interactions(mat = A1, type = int_type);
 diag(A1) <- -1;
+t1       <- rand_mat_ga(A1, max_it = 100, converg = 0.00001);
+hist(t1[1,], xlim = c(0, 1), breaks = 20);
 
-rind  <- runif(n = nn*1000, min = 0, max = 1);
-inds  <- matrix(data = rind, nrow = 1000, ncol = nn);
-
-ress  <- NULL;
-iters <- 2000;
-iter  <- iters;
-while(iter > 0){
-    ivar  <- rep(x = 0, length = dim(inds)[1]);
-    ifit  <- rep(x = 0, length = dim(inds)[1]);
-    isst  <- rep(x = 0, length = dim(inds)[1]);
-    for(i in 1:dim(inds)[1]){
-        ifit[i] <- -1*max(Re(eigen(inds[i,]*A1)$values));
-        ivar[i] <- var(inds[i,]);
-        isst[i] <- max(Re(eigen(inds[i,]*A1)$values)) < 0;
+rand_mat_ga <- function(A1, max_it = 20, converg = 0.01){
+    nn       <- dim(A1)[1];
+    rind     <- runif(n = nn*1000, min = 0, max = 1);
+    inds     <- matrix(data = rind, nrow = 1000, ncol = nn);
+    lastf    <- -10;
+    ccrit    <- 10;
+    find_st  <- 0;
+    iter     <- max_it;
+    while(iter > 0 & find_st < 1 & ccrit > converg){
+        ivar  <- rep(x = 0, length = dim(inds)[1]);
+        ifit  <- rep(x = 0, length = dim(inds)[1]);
+        isst  <- rep(x = 0, length = dim(inds)[1]);
+        for(i in 1:dim(inds)[1]){
+            ifit[i] <- -1*max(Re(eigen(inds[i,]*A1)$values));
+            ivar[i] <- var(inds[i,]);
+            isst[i] <- max(Re(eigen(inds[i,]*A1)$values)) < 0;
+        }
+        most_fit <- order(ifit, decreasing = TRUE)[1:20];
+        parents  <- inds[most_fit,];
+        new_gen  <- matrix(data = t(parents), nrow = 1000, ncol = nn, 
+                           byrow = TRUE);
+        mu_dat   <- rbinom(n = nn*1000, size = 1, prob = 0.3);
+        mu_dat2  <- rnorm(n = nn*1000, mean = 0, sd = 0.02);
+        mu_dat2[mu_dat2 < 0] <- -mu_dat2[mu_dat2 < 0];
+        mu_dat2[mu_dat2 > 1] <- 1 - mu_dat2[mu_dat2 > 1] %% 1;
+        mu_dat3  <- mu_dat * mu_dat2;
+        mu_mat   <- matrix(data = mu_dat3, nrow = 1000, ncol = nn);
+        new_gen  <- new_gen + mu_mat;
+        new_gen  <- crossover(inds = new_gen, pr = 0.2);
+        inds     <- new_gen;
+        find_st  <- max(isst);
+        newf     <- mean(ifit);
+        ccrit    <- newf - lastf;
+        #print(c(iter, newf, lastf, mean(ivar), find_st));
+        lastf    <- newf;
+        iter     <- iter - 1;
     }
-    most_fit <- order(ifit, decreasing = TRUE)[1:20];
-    parents  <- inds[most_fit,];
-    new_gen  <- matrix(data = t(parents), nrow = 1000, ncol = nn, byrow = TRUE)
-    mu_dat   <- rbinom(n = nn*1000, size = 1, prob = 0.2);
-    mu_dat2  <- rnorm(n = nn*1000, mean = 0, sd = 0.02);
-    mu_dat2[mu_dat2 < 0] <- -mu_dat2[mu_dat2 < 0];
-    mu_dat3  <- mu_dat * mu_dat2;
-    mu_mat   <- matrix(data = mu_dat3, nrow = 1000, ncol = nn);
-    new_gen  <- new_gen + mu_mat;
-    new_gen  <- crossover(inds = new_gen, pr = 0.1);
-    inds     <- new_gen;
-    gen_res  <- c(mean(ifit), mean(ivar), sum(isst)/1000);
-    ress     <- rbind(ress, gen_res);
-    print(c(iters - iter, gen_res));
-    iter <- iter - 1;
+    return(find_st);
 }
 
 crossover <- function(inds, pr = 0.1){
@@ -1145,8 +1156,205 @@ crossover <- function(inds, pr = 0.1){
     return(inds);
 }
 
+rand_gen_var <- function(max_sp, iters, int_type = 0, rmx = 0.4, eps_max = 999){
+  tot_res <- NULL;
+  fea_res <- NULL;
+  nea_res <- NULL;
+  for(i in 2:max_sp){
+    nn             <- i;
+    A1_stt         <- 0;
+    A2_stt         <- 0;
+    A1_fet         <- 0;
+    A2_fet         <- 0;
+    iter           <- iters;
+    tot_res[[i-1]] <- matrix(data = 0, nrow = iter, ncol = 3);
+    fea_res[[i-1]] <- matrix(data = 0, nrow = iter, ncol = 2);
+    while(iter > 0){
+      r_vec    <- rnorm(n = nn, mean = 0, sd = rmx);
+      A1_dat   <- rnorm(n = nn * nn, mean = 0, sd = 0.4);
+      A1       <- matrix(data = A1_dat, nrow = nn, ncol = nn);
+      A1       <- species_interactions(mat = A1, type = int_type);
+      diag(A1) <- -1;
+      epsil    <- runif(n = nn, min = 1, max = eps_max);
+      eps_dat  <- rep(x = epsil, times = nn);
+      eps_mat  <- matrix(data = epsil, nrow = nn, ncol = nn, 
+                         byrow = FALSE);
+      avg_mat  <- matrix(data = mean(1:eps_max), nrow = nn, ncol = nn, 
+                         byrow = FALSE);
+      A2       <- A1 * eps_mat;
+      A3_stb   <- rand_mat_ga(A1, max_it = 100, converg = 0.00001);
+      A1       <- A1 * avg_mat;
+      
+      A1_stb   <- max(Re(eigen(A1)$values)) < 0;
+      A2_stb   <- max(Re(eigen(A2)$values)) < 0;
+      A1_fea   <- min(-1*solve(A1) %*% r_vec) > 0;
+      A2_fea   <- min(-1*solve(A2) %*% r_vec) > 0;
 
-plot(x = 1:iters, y = ress[,2], type = "l", lwd = 2);
+      if(A1_stb == TRUE){
+        tot_res[[i-1]][iter, 1] <- 1;
+      }
+      if(A2_stb == TRUE){
+        tot_res[[i-1]][iter, 2] <- 1;
+      }
+      if(A3_stb == 1){
+        tot_res[[i-1]][iter, 3] <- 1;
+      }
+      if(A1_fea == TRUE){
+        fea_res[[i-1]][iter, 1] <- 1;
+      }
+      if(A2_fea == TRUE){
+        fea_res[[i-1]][iter, 2] <- 1;
+      }
+      iter    <- iter - 1;
+    }
+    print(i);
+  }
+  all_res <- summarise_randmat(tot_res = tot_res, fea_res = fea_res);
+  return(all_res);
+}
+
+
+species_interactions <- function(mat, type = 0){
+  if(type == 1){
+    mat[mat > 0] <- -1*mat[mat > 0];
+  }
+  if(type == 2){
+    mat[mat < 0] <- -1*mat[mat < 0];
+  }
+  if(type == 3){
+    for(i in 1:dim(mat)[1]){
+      for(j in 1:dim(mat)[2]){
+        if(mat[i, j] * mat[j, i] > 0){
+          mat[j, i] <- -1 * mat[j, i];
+        }
+      }
+    }
+  }
+  return(mat);
+}
+
+
+summarise_randmat <- function(tot_res, fea_res){
+  sims    <- length(tot_res);
+  all_res <- matrix(data = 0, nrow = sims, ncol = 17);
+  for(i in 1:sims){
+    all_res[i, 1]  <- i + 1;
+    all_res[i, 2]  <- sum(tot_res[[i]][,1] == FALSE);
+    all_res[i, 3]  <- sum(tot_res[[i]][,1] == TRUE);
+    all_res[i, 4]  <- sum(tot_res[[i]][,2] == FALSE);
+    all_res[i, 5]  <- sum(tot_res[[i]][,2] == TRUE);
+    all_res[i, 6]  <- sum(tot_res[[i]][,3] == FALSE);
+    all_res[i, 7]  <- sum(tot_res[[i]][,3] == TRUE);
+    all_res[i, 8]  <- sum(tot_res[[i]][,4] == FALSE);
+    all_res[i, 9]  <- sum(tot_res[[i]][,4] == TRUE);
+    all_res[i, 10] <- sum(fea_res[[i]][,1] == FALSE);
+    all_res[i, 11] <- sum(fea_res[[i]][,1] == TRUE);
+    all_res[i, 12] <- sum(fea_res[[i]][,2] == FALSE);
+    all_res[i, 13] <- sum(fea_res[[i]][,2] == TRUE);
+    all_res[i, 14] <- sum(fea_res[[i]][,3] == FALSE);
+    all_res[i, 15] <- sum(fea_res[[i]][,3] == TRUE);
+    all_res[i, 16] <- sum(fea_res[[i]][,4] == FALSE);
+    all_res[i, 17] <- sum(fea_res[[i]][,4] == TRUE);
+  }
+  return(all_res);
+}
+
+
+make_gammas <- function(nn = 10, distribution = 1, mn = 10, sdd = 1){
+    if(distribution == 0){
+        dat          <- rep(x = mn, times = nn);
+    }
+    if(distribution == 1){
+        dat          <- rnorm(n = nn, mean = mn, sd = sdd);
+        dat[dat < 0] <- -dat[dat < 0];
+    }
+    if(distribution == 2){
+        dat          <- runif(n = nn, min = 0, max = 20);
+        dat          <- sdd * (dat / sd(dat));
+    }
+    if(distribution == 3){
+        dat          <- sdd * rexp(n = nn);
+    }
+    if(distribution == 4){
+        dat          <- 1 - (sdd * rexp(n = nn));
+        dat          <- dat - min(dat);
+        dat          <- dat - mean(dat) + mn;
+        dat[dat < 0] <- -dat[dat < 0];
+    }
+    return(dat);
+}
+
+
+
+rand_gen_var <- function(max_sp, iters, int_type = 0, rmx = 0.4, eps_max = 999){
+    tot_res <- NULL;
+    fea_res <- NULL;
+    for(i in 2:max_sp){
+        nn             <- i;
+        A1_stt         <- 0;
+        A2_stt         <- 0;
+        A1_fet         <- 0;
+        A2_fet         <- 0;
+        iter           <- iters;
+        tot_res[[i-1]] <- matrix(data = 0, nrow = iter, ncol = 4);
+        fea_res[[i-1]] <- matrix(data = 0, nrow = iter, ncol = 4);
+        while(iter > 0){
+            r_vec    <- rnorm(n = nn, mean = 0, sd = rmx);
+            A1_dat   <- rnorm(n = nn * nn, mean = 0, sd = 0.4);
+            A1       <- matrix(data = A1_dat, nrow = nn, ncol = nn);
+            A1       <- species_interactions(mat = A1, type = int_type);
+            diag(A1) <- -1;
+            gam0     <- make_gammas(nn = i, distribution = 0);
+            gam1     <- make_gammas(nn = i, distribution = 1);
+            gam2     <- make_gammas(nn = i, distribution = 2);
+            gam3     <- make_gammas(nn = i, distribution = 3);
+            gam4     <- make_gammas(nn = i, distribution = 4);
+            A2       <- A1 * gam1;
+            A3       <- A1 * gam2;
+            A4       <- A1 * gam3;
+            A1       <- A1 * gam0;
+            A1_stb   <- max(Re(eigen(A1)$values)) < 0;
+            A2_stb   <- max(Re(eigen(A2)$values)) < 0;
+            A3_stb   <- max(Re(eigen(A3)$values)) < 0;
+            A4_stb   <- max(Re(eigen(A4)$values)) < 0;
+            A1_fea   <- min(-1*solve(A1) %*% r_vec) > 0;
+            A2_fea   <- min(-1*solve(A2) %*% r_vec) > 0;
+            A3_fea   <- min(-1*solve(A3) %*% r_vec) > 0;
+            A4_fea   <- min(-1*solve(A4) %*% r_vec) > 0;
+            if(A1_stb == TRUE){
+                tot_res[[i-1]][iter, 1] <- 1;
+            }
+            if(A2_stb == TRUE){
+                tot_res[[i-1]][iter, 2] <- 1;
+            }
+            if(A3_stb == TRUE){
+                tot_res[[i-1]][iter, 3] <- 1;
+            }
+            if(A4_stb == TRUE){
+                tot_res[[i-1]][iter, 4] <- 1;
+            }
+            if(A1_fea == TRUE){
+                fea_res[[i-1]][iter, 1] <- 1;
+            }
+            if(A2_fea == TRUE){
+                fea_res[[i-1]][iter, 2] <- 1;
+            }
+            if(A3_fea == TRUE){
+                fea_res[[i-1]][iter, 3] <- 1;
+            }
+            if(A4_fea == TRUE){
+                fea_res[[i-1]][iter, 4] <- 1;
+            }
+            iter    <- iter - 1;
+        }
+        print(i);
+    }
+    all_res <- summarise_randmat(tot_res = tot_res, fea_res = fea_res);
+    return(all_res);
+}
+
+
+
 ################################################################################
 
 
