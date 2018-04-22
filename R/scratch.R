1b7ca8575de75103b95bd8f727163aaea23fe6e9
bradduthie <brad.duthie@gmail.com>
2018-04-22 17:58:07 +0100

    some scratch ideas

--- R/scratch.R
+++ R/scratch.R
@@ -9,7 +9,7 @@ rand_gen_var <- function(max_sp, iters, int_type = 0, rmx = 0.4, gamma_sd = 1,
     tot_res[[i-1]] <- matrix(data = 0, nrow = iter, ncol = 7);
     fea_res[[i-1]] <- matrix(data = 0, nrow = iter, ncol = 7);
     while(iter > 0){
-      r_vec    <- rnorm(n = i, mean = 0, sd = rmx);
+      r_vec    <- runif(n = i, min = -1, max = 0); #rnorm(n = i, mean = 0, sd = rmx);
       A0_dat   <- rnorm(n = i * i, mean = 0, sd = 0.4);
       A0       <- matrix(data = A0_dat, nrow = i, ncol = i);
       A0       <- species_interactions(mat = A0, type = int_type);
@@ -18,15 +18,26 @@ rand_gen_var <- function(max_sp, iters, int_type = 0, rmx = 0.4, gamma_sd = 1,
       A0       <- A0 * C_mat;
       diag(A0) <- -1;
       gam0     <- make_gammas(nn = i, distribution = 0, sdd = gamma_sd);
-      gam1     <- make_gammas(nn = i, distribution = 1, sdd = gamma_sd);
+      
+      gam1     <- runif(n = i, min = 0, max = 999) #make_gammas(nn = i, distribution = 1, sdd = gamma_sd);
+      gam0     <- rep(mean(gam1), i);
+      
       gam2     <- make_gammas(nn = i, distribution = 2, sdd = gamma_sd);
       gam3     <- make_gammas(nn = i, distribution = 3, sdd = gamma_sd);
       gam4     <- make_gammas(nn = i, distribution = 4, sdd = gamma_sd);
+      
       A1       <- A0 * gam1;
-      A2       <- A0 * gam2;
-      A3       <- A0 * gam3;
+      A2       <- A0 * gam1;
+      A3       <- A0 * gam0;
+      diag(A2) <- gam0;
+      diag(A3) <- gam1;
+      
+
       A4       <- A0 * gam4;
+      
+      
       A0       <- A0 * gam0;
+      
       A0_stb   <- max(Re(eigen(A0)$values)) < 0;
       A1_stb   <- max(Re(eigen(A1)$values)) < 0;
       A2_stb   <- max(Re(eigen(A2)$values)) < 0;
@@ -67,8 +78,12 @@ rand_gen_var <- function(max_sp, iters, int_type = 0, rmx = 0.4, gamma_sd = 1,
       if(A4_fea == TRUE){
         fea_res[[i-1]][iter, 5] <- 1;
       }
-      new_v   <- get_var_A(A1);
-      vars_t  <- rbind(vars_t, c(i, new_v));
+      v0      <- get_var_A(A0);
+      v1      <- get_var_A(A1);
+      v2      <- get_var_A(A2);
+      v3      <- get_var_A(A3);
+      v4      <- get_var_A(A4);
+      vars_t  <- rbind(vars_t, c(i, v0, v1, v2, v3, v4));
       iter    <- iter - 1;
     }
     print(i);
@@ -79,22 +94,29 @@ rand_gen_var <- function(max_sp, iters, int_type = 0, rmx = 0.4, gamma_sd = 1,
 
 get_var_A <- function(A){
   off_vec  <- NULL;
-  diag_vec <- NULL;
   for(i in 1:dim(A)[1]){
     for(j in 1:dim(A)[2]){
-      if(i == j){
-        diag_vec <- c(diag_vec, A[i,j]);
-      }else{
+      if(i != j){
         off_vec  <- c(off_vec, A[i, j])
       }
     }
   }
-  v_diag <- var(diag_vec);
   v_off  <- var(off_vec);
-  vals   <- c(v_diag, v_off);
-  return(vals);
+  return(v_off);
 }
 
+get_off_corr <- function(A){
+  pairs <- NULL;
+  for(i in 1:dim(A)[1]){
+    for(j in 1:dim(A)[2]){
+      if(i > j){
+        pairs  <- rbind(pairs, c(A[j, i], A[i, j]));
+      }
+    }
+  }
+  rho <- cor(pairs[,1], pairs[,2]);
+  return(rho);
+}
 
 summ <- NULL;
 for(i in 2:30){
