249351d0d9b46dfec6437cd9cafd106ed1faf974
Brad Duthie <brad.duthie@gmail.com>
2020-01-02 15:40:57 +0000

    For the small world network

--- R/scratch.R
+++ R/scratch.R
@@ -1,66 +1,60 @@
-
-
-create_swn <- function(N = 100, K = 20, beta = 0.05){
-    mat <- matrix(data = 0, nrow = N, ncol = N);
-    diag(mat) <- 1;
-    if(K %% 2 != 0){
-        warning("K needs to be an even integer");
-    }
-    if(K > dim(mat)[1]){
-        warning("K is too high");
-    }
-    mat <- add_sw_edges(mat, K);
-    mat <- rewire_sw_edges(mat, K, beta);
-    return(mat);
-}
-
-add_sw_edges <- function(mat, K){
-    N <- dim(mat)[1];
-    for(i in 1:N){
-        for(j in 1:N){
-            if(i > j){
-                neig1 <- abs(i - j);
-                neig2 <- abs(i - (N+j));
-                neig  <- min(c(neig1, neig2));
-                if(0 < neig & neig <= (K/2)){
-                    mat[i, j] <- 1;
-                    mat[j, i] <- 1;
-                }
+rand_gen_var <- function(max_sp, iters, int_type = 0, rmx = 0.4, C = 1, by = 1,
+                         sigma = 0.4){
+    tot_res <- NULL;
+    fea_res <- NULL;
+    real_Cs <- NULL;
+    sp_try  <- seq(from = by, to = max_sp, by = by);
+    for(i in 1:length(sp_try)){
+        iter           <- iters;
+        tot_res[[i]]   <- matrix(data = 0, nrow = iter, ncol = 7);
+        fea_res[[i]]   <- matrix(data = 0, nrow = iter, ncol = 7);
+        real_Cs[[i]]   <- matrix(data = 0, nrow = iter, ncol = 3);
+        while(iter > 0){
+            r_vec    <- rnorm(n = sp_try[i], mean = 0, sd = rmx);
+            A0_dat   <- rnorm(n = sp_try[i] * sp_try[i], mean = 0, sd = sigma);
+            A0       <- matrix(data = A0_dat, nrow = sp_try[i], 
+                               ncol = sp_try[i]);
+            A0       <- species_interactions(mat = A0, type = int_type);
+            swn      <- create_swn(N = sp_try[i], beta = 0.2,
+                                   K = (sp_try[i] / 8));
+            real_Cs[[i]][iter, 1] <- sp_try[i];
+            real_Cs[[i]][iter, 2] <- iter;
+            real_Cs[[i]][iter, 3] <- get_C(swn);
+            C_dat    <- rbinom(n = sp_try[i] * sp_try[i], size = 1, prob = C);
+            C_mat    <- matrix(data = C_dat, nrow = sp_try[i], 
+                               ncol = sp_try[i]);
+            A0       <- A0 * C_mat;
+            A0       <- A0 * swn;
+            diag(A0) <- -1;
+            gam1     <- runif(n = sp_try[i], min = 0, max = 2);
+            A1       <- A0 * gam1;
+            A0       <- A0 * mean(gam1);
+            A0_stb   <- max(Re(eigen(A0)$values)) < 0;
+            A1_stb   <- max(Re(eigen(A1)$values)) < 0;
+            A0_fea   <- min(-1*solve(A0) %*% r_vec) > 0;
+            A1_fea   <- min(-1*solve(A1) %*% r_vec) > 0;
+            if(A0_stb == TRUE){
+                tot_res[[i]][iter, 1] <- 1;
             }
-       }
-    }
-    return(mat);
-}
-
-rewire_sw_edges <- function(mat, K, beta){
-    N <- dim(mat)[1];
-    for(i in 1:N){
-        for(j in 1:N){
-            if(i != j){
-                samp_beta <- runif(n = 1) < beta;
-                if(samp_beta){ # Rewire node
-                    mat[i, j] <- 0;
-                    mat[j, i] <- 0;
-                    k         <- sample_k(i, N, K);
-                    mat[i, k] <- 1;
-                    mat[k, i] <- 1;
-                }
+            if(A1_stb == TRUE){
+                tot_res[[i]][iter, 2] <- 1;
             }
+            if(A0_fea == TRUE){
+                fea_res[[i]][iter, 1] <- 1;
+            }
+            if(A1_fea == TRUE){
+                fea_res[[i]][iter, 2] <- 1;
+            }
+            iter    <- iter - 1;
         }
+        print(sp_try[i]);
     }
-    return(mat);
+    all_res     <- summarise_randmat(tot_res = tot_res, fea_res = fea_res);
+    all_res[,1] <- sp_try;
+    return(list(all_res = all_res, real_Cs = real_Cs));
 }
 
-sample_k <- function(i, N, K){
-    tsamp <- 1:N;
-    lo    <- i - (K/2);
-    hi    <- i + (K/2);
-    span1 <- seq(from = lo, to = hi, by = 1);
-    span2 <- span1 + N;
-    span  <- c(span1, span2);
-    nouse <- span[span %in% tsamp];
-    tsamp <- tsamp[-nouse];
-    getit <- sample(tsamp, size = 1);
-    return(getit);
-}
+
+sim <- rand_gen_var(max_sp = 256, iters = 10000, by = 16);
+
 
