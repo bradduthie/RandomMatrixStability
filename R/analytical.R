f210c3bc1df391c73189c5ac1d0789a8e8dab4d5
Brad Duthie <brad.duthie@gmail.com>
2020-01-09 08:33:37 +0000

    Some more plots

--- R/analytical.R
+++ R/analytical.R
@@ -191,3 +191,248 @@ y1 <- sigma * 1.99 * sqrt(S/2) * sin(vl);
 points(x = x1, y = y1, type = "l", lwd = 3, col = "orange");
 
 # Need to figure out where that variation is going.
+
+
+################################################################################
+# See the relationship between rho and the leading eigenvalue real component
+################################################################################
+rand_rho_var <- function(S, rhos, iters, int_type = 0, rmx = 0.4, C = 1, 
+                         by = 1, sigma = 0.4){
+    tot_res <- NULL;
+    fea_res <- NULL;
+    rho_res <- NULL;
+    cmplxty <- NULL;
+    for(i in 1:length(rhos)){
+        iter           <- iters;
+        tot_res[[i]]   <- matrix(data = 0, nrow = iter, ncol = 8);
+        fea_res[[i]]   <- matrix(data = 0, nrow = iter, ncol = 7);
+        rho_res[[i]]   <- matrix(data = 0, nrow = iter, ncol = 6);
+        cmplxty[[i]]   <- matrix(data = 0, nrow = iter, ncol = 2);
+        while(iter > 0){
+            r_vec    <- rnorm(n = S, mean = 0, sd = rmx);
+            A0       <- build_rho_mat(S = S, sigma = sigma, rho = rhos[i]);
+            gam1     <- runif(n = S, min = 0, max = 2);
+            A1       <- A0 * gam1;
+            A0       <- A0 * mean(gam1);
+            A0_stb   <- max(Re(eigen(A0)$values)) < 0;
+            A1_stb   <- max(Re(eigen(A1)$values)) < 0;
+            A0_fea   <- min(-1*solve(A0) %*% r_vec) > 0;
+            A1_fea   <- min(-1*solve(A1) %*% r_vec) > 0;
+            A0_rho   <- mat_rho(A0);
+            A1_rho   <- mat_rho(A1);
+            rho_diff <- A1_rho - A0_rho;
+            rho_abs  <- abs(A1_rho) - abs(A0_rho);
+            if(A0_stb == TRUE){
+                tot_res[[i]][iter, 1] <- 1;
+            }
+            if(A1_stb == TRUE){
+                tot_res[[i]][iter, 2] <- 1;
+            }
+            if(A0_fea == TRUE){
+                fea_res[[i]][iter, 1] <- 1;
+            }
+            if(A1_fea == TRUE){
+                fea_res[[i]][iter, 2] <- 1;
+            }
+            rho_res[[i]][iter, 1] <- A0_rho;
+            rho_res[[i]][iter, 2] <- A1_rho;
+            rho_res[[i]][iter, 3] <- rho_diff;
+            rho_res[[i]][iter, 4] <- rho_abs;
+            rho_res[[i]][iter, 5] <- max(Re(eigen(A0)$values));
+            rho_res[[i]][iter, 6] <- max(Re(eigen(A1)$values));
+            cmplxty[[i]][iter, 1] <- get_complexity(A0);
+            cmplxty[[i]][iter, 2] <- get_complexity(A1);
+            iter                  <- iter - 1;
+        }
+        print(rhos[i]);
+    }
+    all_res     <- summarise_randmat(tot_res = tot_res, fea_res = fea_res,
+                                     rho_res = rho_res, cmplxty = cmplxty);
+    all_res[,1]          <- rhos;
+    colnames(all_res)[1] <- "rho_set";
+    return(all_res);
+}
+
+simpleboot <- function(freqs,repli=1000,alpha=0.05){    
+    vals  <- NULL;                                                          
+    i     <- 0;                                         
+    while(i < repli){                                                   
+        boot  <- sample(x=freqs,size=length(freqs),replace=TRUE);           
+        strap <- mean(boot);                                                
+        vals  <- c(vals,strap);                                             
+        i     <- i + 1;                                                     
+    }                                                                       
+    vals   <- sort(x=vals,decreasing=FALSE);                            
+    lowCI  <- vals[round((alpha*0.5)*repli)];                               
+    highCI <- vals[round((1-(alpha*0.5))*repli)];                           
+    CIs    <- c(lowCI,highCI);                                          
+    return(CIs);                                                            
+}  
+
+summarise_randmat <- function(tot_res, fea_res, rho_res, cmplxty){
+    sims    <- length(tot_res);
+    all_res <- matrix(data = 0, nrow = sims, ncol = 25);
+    for(i in 1:sims){
+        all_res[i, 1]  <- i + 1;
+        # Stable and unstable
+        all_res[i, 2]  <- sum(tot_res[[i]][,1] == FALSE);
+        all_res[i, 3]  <- sum(tot_res[[i]][,1] == TRUE);
+        all_res[i, 4]  <- sum(tot_res[[i]][,2] == FALSE);
+        all_res[i, 5]  <- sum(tot_res[[i]][,2] == TRUE);
+        # Stabilised and destabilised
+        all_res[i, 6] <- sum(tot_res[[i]][,1] == FALSE & 
+                                 tot_res[[i]][,2] == TRUE);
+        all_res[i, 7] <- sum(tot_res[[i]][,1] == TRUE & 
+                                 tot_res[[i]][,2] == FALSE);
+        # Feasible and infeasible
+        all_res[i, 8]  <- sum(fea_res[[i]][,1] == FALSE);
+        all_res[i, 9]  <- sum(fea_res[[i]][,1] == TRUE);
+        all_res[i, 10]  <- sum(fea_res[[i]][,2] == FALSE);
+        all_res[i, 11]  <- sum(fea_res[[i]][,2] == TRUE);
+        # Feased and defeased
+        all_res[i, 12] <- sum(fea_res[[i]][,1] == FALSE & 
+                                  fea_res[[i]][,2] == TRUE);
+        all_res[i, 13] <- sum(fea_res[[i]][,1] == TRUE & 
+                                  fea_res[[i]][,2] == FALSE);
+        # Mean correlations between A[i,j] and A[j,i] off-diag elements
+        all_res[i, 14] <- mean(rho_res[[i]][,1]);
+        all_res[i, 15] <- mean(rho_res[[i]][,2]);
+        all_res[i, 16] <- mean(rho_res[[i]][,3]);
+        all_res[i, 17] <- mean(rho_res[[i]][,4]);
+        all_res[i, 18] <- mean(cmplxty[[i]][,1]);
+        all_res[i, 19] <- mean(cmplxty[[i]][,2]);
+        all_res[i, 20] <- mean(rho_res[[i]][,5]);
+        all_res[i, 21] <- mean(rho_res[[i]][,6]);
+        all_res[i, 22:23] <- range(rho_res[[i]][,5]);
+        all_res[i, 24:25] <- range(rho_res[[i]][,6]);
+    }
+    cnames <- c("N", "A0_unstable", "A0_stable", "A1_unstable", "A1_stable", 
+                "A1_stabilised", "A1_destabilised", "A0_infeasible", 
+                "A0_feasible", "A1_infeasible", "A1_feasible", 
+                "A1_made_feasible", "A1_made_infeasible", "A0_rho", "A1_rho",
+                "rho_diff", "rho_abs", "complex_A0", "complex_A1", "A0_eig",
+                "A1_eig", "LCI_A0", "UCI_A0", "LCI_A1", "UCI_A1");
+    colnames(all_res) <- cnames;
+    return(all_res);
+}
+
+sim10 <- rand_rho_var(S = 10, rhos = seq(from = -0.5, to = 0.5, by = 0.05), 
+                      iters = 10000);
+
+sim15 <- rand_rho_var(S = 15, rhos = seq(from = -0.5, to = 0.5, by = 0.05), 
+                      iters = 10000);
+
+sim20 <- rand_rho_var(S = 20, rhos = seq(from = -0.5, to = 0.5, by = 0.05), 
+                      iters = 10000);
+
+sim25 <- rand_rho_var(S = 25, rhos = seq(from = -0.5, to = 0.5, by = 0.05), 
+                      iters = 10000);
+
+sim30 <- rand_rho_var(S = 30, rhos = seq(from = -0.5, to = 0.5, by = 0.05), 
+                      iters = 10000);
+
+sim35 <- rand_rho_var(S = 35, rhos = seq(from = -0.5, to = 0.5, by = 0.05), 
+                      iters = 10000);
+
+# Completely linear
+par(mar = c(1, 1, 1, 1), mfrow = c(3, 2), oma = c(5, 5, 1, 1));
+
+plot(x = sim10[,1], y = sim10[,20], type = "b", pch = 20, lwd = 2, xaxt = "n");
+points(x = sim10[,15], y = sim10[,21], type = "b", lwd = 2);
+
+plot(x = sim15[,1], y = sim15[,20], type = "b", pch = 20, lwd = 2, xaxt = "n",
+     yaxt = "n");
+points(x = sim15[,15], y = sim15[,21], type = "b", lwd = 2);
+
+plot(x = sim20[,1], y = sim20[,20], type = "b", pch = 20, lwd = 2, xaxt = "n");
+points(x = sim20[,15], y = sim20[,21], type = "b", lwd = 2);
+
+plot(x = sim25[,1], y = sim25[,20], type = "b", pch = 20, lwd = 2, xaxt = "n",
+     yaxt = "n");
+points(x = sim25[,15], y = sim25[,21], type = "b", lwd = 2);
+
+plot(x = sim30[,1], y = sim30[,20], type = "b", pch = 20, lwd = 2);
+points(x = sim30[,15], y = sim30[,21], type = "b", lwd = 2);
+
+plot(x = sim35[,1], y = sim35[,20], type = "b", pch = 20, lwd = 2, yaxt = "n");
+points(x = sim35[,15], y = sim35[,21], type = "b", lwd = 2);
+
+mtext(text = "E correlation between A_ij and A_ji", side = 1,
+      line = 0, outer = TRUE, cex = 1.5);
+mtext(text = "E leading real part eigenvalue", side = 2,
+      line = 1, outer = TRUE, cex = 1.5);
+
+
+
+
+
+
+par(mar = c(1, 1, 1, 1), mfrow = c(3, 2), oma = c(5, 5, 1, 1));
+
+plot(x = sim10[,1], y = sim10[,20], type = "b", pch = 20, lwd = 2, xaxt = "n");
+points(x = sim10[,15], y = sim10[,21], type = "b", lwd = 2);
+abline(h = 0, col = "red");
+
+plot(x = sim15[,1], y = sim15[,20], type = "b", pch = 20, lwd = 2, xaxt = "n");
+points(x = sim15[,15], y = sim15[,21], type = "b", lwd = 2);
+abline(h = 0, col = "red");
+
+plot(x = sim20[,1], y = sim20[,20], type = "b", pch = 20, lwd = 2, xaxt = "n");
+points(x = sim20[,15], y = sim20[,21], type = "b", lwd = 2);
+abline(h = 0, col = "red");
+
+plot(x = sim25[,1], y = sim25[,20], type = "b", pch = 20, lwd = 2, xaxt = "n");
+points(x = sim25[,15], y = sim25[,21], type = "b", lwd = 2);
+abline(h = 0, col = "red");
+
+plot(x = sim30[,1], y = sim30[,20], type = "b", pch = 20, lwd = 2);
+points(x = sim30[,15], y = sim30[,21], type = "b", lwd = 2);
+abline(h = 0, col = "red");
+
+plot(x = sim35[,1], y = sim35[,20], type = "b", pch = 20, lwd = 2);
+points(x = sim35[,15], y = sim35[,21], type = "b", lwd = 2);
+abline(h = 0, col = "red");
+
+mtext(text = "E correlation between A_ij and A_ji", side = 1,
+      line = 0, outer = TRUE, cex = 1.5);
+mtext(text = "E leading real part eigenvalue", side = 2,
+      line = 1, outer = TRUE, cex = 1.5);
+
+
+
+
+
+
+
+
+
+
+rand_fishing <- function(S, iters, int_type = 0, rmx = 0.4, C = 1, 
+                         by = 1, sigma = 0.4){
+    tot_res <- NULL;
+    for(i in 1:length(rhos)){
+        iter           <- iters;
+        while(iter > 0){
+            A0_dat   <- rnorm(n = S * S, mean = 0, sd = sigma);
+            A0       <- matrix(data = A0_dat, nrow = S, ncol = S);
+            gam1     <- runif(n = S, min = 0, max = 2);
+            A1       <- A0 * gam1;
+            A0       <- A0 * mean(gam1);
+            A0_eig   <- max(Re(eigen(A0)$values));
+            A1_eig   <- max(Re(eigen(A1)$values));
+            if(A0_eig > 0 & A1_eig < 0){
+                A0_rho   <- mat_rho(A0);
+                A1_rho   <- mat_rho(A1);
+                A0_comp  <- get_complexity(A0);
+                A1_comp  <- get_complexity(A1);
+                it_res   <- c(A0_eig, A1_eig, A0_rho, A1_rho, A0_comp, A1_comp);
+                tot_res  <- rbind(tot_res, it_res);
+            }
+            iter <- iter - 1;
+        }
+    }
+    colnames(tot_res) <- c("A0_eig", "A1_eig", "A0_rho", "A1_rho", "A0_comp",
+                           "A1_comp");
+    return(tot_res);
+}
+
