2942cae8592ae6ab2082e629a8fc63bf6aa22501
Brad Duthie <brad.duthie@gmail.com>
2020-01-23 23:40:05 +0000

    Small change

--- R/utilities.R
+++ R/utilities.R
@@ -174,3 +174,19 @@ get_sigma_sqd <- function(mat){
     return(var_offs);
 }
 
+
+simpleboot <- function(freqs , repli = 1000, alpha = 0.05){    
+    vals  <- NULL;                                                          
+    i     <- 0;                                         
+    while(i < repli){                                                   
+        boot  <- sample(x = freqs, size = length(freqs), replace = TRUE);           
+        strap <- mean(boot);                                                
+        vals  <- c(vals,strap);                                             
+        i     <- i + 1;                                                     
+    }                                                                       
+    vals   <- sort(x = vals, decreasing = FALSE);                            
+    lowCI  <- vals[round((alpha*0.5)*repli)];                               
+    highCI <- vals[round((1-(alpha*0.5))*repli)];                           
+    CIs    <- c(lowCI,highCI);                                          
+    return(CIs);                                                            
+}  
