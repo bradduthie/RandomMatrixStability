731603049543e3eec616ca2b9934d6bff6ff4559
Brad Duthie <brad.duthie@gmail.com>
2020-03-13 17:59:33 +0000

    Fix the legend on the big bar plot figure

--- R/Figures_3_4.R
+++ R/Figures_3_4.R
@@ -29,8 +29,8 @@ plot_stables <- function(dat, S_s = 32){
          cex = 1, col = "black", yaxs="i");
     points(x = x1, y = y1, lwd = 2, type = "l", col = "black");
     axis(side = 4, at = c(0, 0.2, 0.4, 0.6, 0.8, 1.0), cex.axis = 1.5);
-    legend("topleft", c(expression(paste(gamma," = 1")), 
-                        expression(paste("Var(",gamma,")"))), 
+    legend("topleft", c(expression(paste(sigma[gamma]^2," = 0")), 
+                        expression(paste(sigma[gamma]^2," = 1/3"))), 
            pch=15, col=c("dodgerblue4","firebrick"), cex = 1.5, horiz = TRUE);
     #=================================
     mtext(side = 1, text = "System size (S)", cex = 2, outer = TRUE, 
@@ -75,8 +75,8 @@ plot_stable_4 <- function(dat, S_s = 40){
          cex = 1, col = "black", yaxs="i");
     points(x = x1, y = y1, lwd = 2, type = "l", col = "black");
     axis(side = 4, at = c(0, 0.2, 0.4, 0.6, 0.8, 1.0), cex.axis = 1.5);
-    legend("topleft", c(expression(paste(gamma," = 1")), 
-                        expression(paste("Var(",gamma,")"))), 
+    legend("topleft", c(expression(paste(sigma[gamma]^2," = 0")), 
+                        expression(paste(sigma[gamma]^2," = 0"))), 
            pch=15, col=c("dodgerblue4","firebrick"), cex = 1.5, horiz = TRUE);
     #=================================
     mtext(side = 1, text = "System size (S)", cex = 2, outer = TRUE, 
@@ -87,9 +87,3 @@ plot_stable_4 <- function(dat, S_s = 40){
         paste("Pr. of systems stable due to Var(",gamma,")")),
         cex = 2, outer = TRUE, line = 3.5);
 }
-
-
-
-
-
-
